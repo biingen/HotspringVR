91a1cc48c3f7ae22bb18b1c32d78b2b0ae940c0b
pingenhsieh <pingen.hsieh@auo.com>
2021-01-14 18:17:28 +0800

    Add the serialport decode.

--- Medical.cs
+++ Medical.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
 
 namespace Hotspring
 {
@@ -29,6 +31,10 @@ namespace Hotspring
         byte[] byteMessage_A = new byte[Math.Max(byteMessage_max_Ascii, byteMessage_max_Hex)];
         int byteMessage_length_A = 0;
 
+        Queue<byte> serialPortA = new Queue<byte>();
+        List<byte> dataListA = new List<byte>();
+        //Queue<List> dataPortA = new Queue<dataListA>();
+
         //        byte command[255];
         //        command[0] = 0x06;
         //        command[1] = 0x01;
@@ -43,6 +49,9 @@ namespace Hotspring
 
         private void hScrollBar_setvalue_Scroll(object sender, ScrollEventArgs e)
         {
+            Thread LogA_analysis = new Thread(new ThreadStart(serialPortA_analysis));
+            Thread LogA_record = new Thread(new ThreadStart(serialPortA_recorder));
+
             byte [] command = new byte[6];
             command[0] = 0x06;
             command[1] = 0x01;
@@ -54,6 +63,8 @@ namespace Hotspring
             if (ini12.INIRead(Config_Path, "serialPort1", "Exist", "") == "1" && serialPort1.IsOpen == false)          //送至Comport
             {
                 Open_serialPort1();
+                LogA_analysis.Start();
+                LogA_record.Start();
                 serialPort1.Write(command,0,command.Length);
             }
             else if (ini12.INIRead(Config_Path, "serialPort1", "Exist", "") == "1" && serialPort1.IsOpen == true)          //送至Comport
@@ -65,6 +76,9 @@ namespace Hotspring
 
         private void button_getvalue_Click(object sender, EventArgs e)
         {
+            Thread LogA_analysis = new Thread(new ThreadStart(serialPortA_analysis));
+            Thread LogA_record = new Thread(new ThreadStart(serialPortA_recorder));
+
             byte[] command = new byte[5];
             command[0] = 0x05;
             command[1] = 0x01;
@@ -75,6 +89,8 @@ namespace Hotspring
             if (ini12.INIRead(Config_Path, "serialPort1", "Exist", "") == "1" && serialPort1.IsOpen == false)          //送至Comport
             {
                 Open_serialPort1();
+                LogA_analysis.Start();
+                LogA_record.Start();
                 serialPort1.Write(command, 0, command.Length);
             }
             else if (ini12.INIRead(Config_Path, "serialPort1", "Exist", "") == "1" && serialPort1.IsOpen == true)          //送至Comport
@@ -84,6 +100,16 @@ namespace Hotspring
         }
 
         public static byte XOR(byte[] bHEX1, int length)
+        {
+            byte bHEX_OUT = bHEX1[0];
+            for (int i = 1; i < length; i++)
+            {
+                bHEX_OUT = (byte)(bHEX_OUT ^ bHEX1[i]);
+            }
+            return bHEX_OUT;
+        }
+
+        public static byte XOR_List(List<byte> bHEX1 , int length)
         {
             byte bHEX_OUT = bHEX1[0];
             for (int i = 1; i < length; i++)
@@ -124,7 +150,7 @@ namespace Hotspring
         }
 
         //  SerialPort分析
-        private void serialPort1_analysis()
+        private void serialPortA_analysis()
         {
             while (serialPort1.IsOpen == true)
             {
@@ -137,27 +163,52 @@ namespace Hotspring
                     for (int index = 0; index < data_to_read; index++)
                     {
                         byte input_ch = dataset[index];
-                        serialPort1_recorder(input_ch);
+                        serialPortA.Enqueue(input_ch);   // Enqueue rs232 data
                     }
                 }
             }
         }
 
         //  SerialPort記錄
-        private void serialPort1_recorder(byte ch, bool SaveToLog = false)
+        private void serialPortA_recorder()
         {
-            if ((ch == 0x0A) || (byteMessage_length_A >= byteMessage_max_Ascii))
+            List<byte> DataLists = new List<byte>();
+            while (serialPort1.IsOpen == true)
             {
-                string dataValue = Encoding.ASCII.GetString(byteMessage_A).Substring(0, byteMessage_length_A);
-                DateTime dt = DateTime.Now;
-                string logValue = "[Receive_serialport1] [" + dt.ToString("yyyy/MM/dd HH:mm:ss.fff") + "]  " + dataValue + "\r\n"; //OK
-                serialPort1_text = string.Concat(serialPort1_text, logValue);
-                byteMessage_length_A = 0;
-            }
-            else
-            {
-                byteMessage_A[byteMessage_length_A] = ch;
-                byteMessage_length_A++;
+                while (serialPortA.Count() > 0)
+                {
+                    if (serialPortA.Peek() == 0xE0)
+                    {
+                        DataLists.Add(serialPortA.Dequeue());
+                        for (int i=3;i< Convert.ToInt32(DataLists[0]);i++)
+                        {
+                            DataLists.Add(serialPortA.Dequeue());
+                        }
+                        byte calculate_checksum = 0x00;
+                        calculate_checksum = XOR_List(DataLists, Convert.ToInt32(DataLists[0]) - 1);
+                        if (calculate_checksum == DataLists[Convert.ToInt32(DataLists[0] - 1)])
+                        {
+                            string strRcv = null;
+                            //int decNum = 0;//存储十进制
+                            for (int i = 0; i < Convert.ToInt32(DataLists[0]); i++) //窗体显示
+                            {
+                                strRcv += DataLists[i].ToString("X2");  //16进制显示
+                            }
+
+                            DateTime dt = DateTime.Now;
+                            string logValue = "[Receive_serialport1] [" + dt.ToString("yyyy/MM/dd HH:mm:ss.fff") + "]  " + strRcv + "\r\n"; //OK
+                            serialPort1_text = string.Concat(serialPort1_text, logValue);
+                            DataLists.Clear();
+                        }
+                    }
+                    else if (DataLists.Count > 3)
+                    {
+                        DataLists.Remove(DataLists[0]);                 //移除掉第一筆的資料
+                        DataLists.Add(serialPortA.Dequeue());          //將queue資料放入list
+                    }
+                    else
+                        DataLists.Add(serialPortA.Dequeue());          //將queue資料放入list
+                }
             }
         }
     }
